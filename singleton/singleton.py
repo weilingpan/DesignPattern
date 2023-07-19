a46f410663a622b3e46305ede0d979d6dac7acc2
WeiLing Pan <zz860530@gmail.com>
2023-07-19 17:16:08 +0800

    feature: 注意變數間的變化

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,3 +1,5 @@
+# reference:https://mark1002.github.io/2018/07/31/python-%E5%AF%A6%E7%8F%BE-singleton-%E6%A8%A1%E5%BC%8F/
+
 class SingleTonNew: 
     _instance = None 
     def __new__(cls, *args, **kwargs): 
@@ -10,6 +12,7 @@ class SingleTonNew:
         self.b = b
 
 s1 = SingleTonNew(a=11, b=21)
+print(id(s1), s1.a, s1.b) #11 21
 s2 = SingleTonNew(a=10, b=2)
-print(id(s1))
-print(id(s2))
+print(id(s1), s1.a, s1.b) #10 2
+print(id(s2), s2.a, s2.b) #10 2
